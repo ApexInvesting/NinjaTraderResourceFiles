b3e477d868b9cc84aa4d612797f15892c9ac3e97
asmith <asmith@asmith.replaytrader.com>
2016-09-01 17:38:13 -0700

    Added VAD template

--- .update.sh
+++ .update.sh
@@ -2,23 +2,23 @@
 
 cur=`git status | head -1 | cut -d" " -f4`
 if [ $cur == alpha ] ; then
-release=prebeta
-if [ ! -z "$1" ] ; then
-    release=$1
-fi
+    release=prebeta
+    if [ ! -z "$1" ] ; then
+	release=$1
+    fi
 
-found=0
-for br in cloud prebeta beta master
-do
-    if [ $found -eq 1 ] ; then
-	break
-    fi
-    if [ $br == $release ] ; then
-	found=1
-    fi
-    git checkout $br
-    git merge alpha
-done
+    found=0
+    for br in cloud prebeta beta master
+    do
+	if [ $found -eq 1 ] ; then
+	    break
+	fi
+	if [ $br == $release ] ; then
+	    found=1
+	fi
+	git checkout $br
+	git merge alpha
+    done
 else
     echo Wrong current branch - $cur
 fi
